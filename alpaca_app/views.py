4d9b68b7ad005d19059bc1ca41915fa0d3372160
Ashley-Pritchard <ashley.pritchard@ouh.nhs.uk>
2020-02-27 16:45:58 +0000

    create new brach for search test

--- alpaca_app/views.py
+++ alpaca_app/views.py
@@ -13,4 +13,12 @@ def import_variant(request):
 	return render(request, 'import_variant.html')
 
 def imported(request):
+	
+	patient = Patient()
+	patient.first_name = request.POST.get('first_name')
+	patient.last_name = request.POST.get('last_name')
+	patient.age = request.POST.get('age')
+	patient.proband = request.POST.get('proband')
+	patient.affected_relatives = request.POST.get('affected_relatives')
+
 	return render(request, 'imported.html')
