f36113ece5cf20ea0163c192770e14fa8997c863
Helen-Warren <helen.warren4@nhs.net>
2020-02-28 11:58:35 +0000

    Resolve merge conflict

--- alpaca_app/views.py
+++ alpaca_app/views.py
@@ -207,4 +207,14 @@ def import_variant(request):
 	return render(request, 'import_variant.html')
 
 def imported(request):
+	
+	gene = Gene.objects.get(id=1)
+	variant = Variant()
+	variant.p_name = request.POST.get('p_name')
+	variant.g_name = request.POST.get('g_name')
+	variant.c_name = request.POST.get('c_name')
+	variant.gene_id = gene
+	variant.classification = request.POST.get('classification')
+	variant.save()
+
 	return render(request, 'imported.html')
